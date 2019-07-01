b3aa3dbf184054e64045d3c5f2a70cb5211d7f91
Jason Wong <jasonwonghl@yahoo.com.sg>
2019-07-02 00:09:06 +0800

    Add RestaurantList

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,15 +1,17 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation'
+import RestaurantList from '../Containers/RestaurantList'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = createStackNavigator({
+  RestaurantList: { screen: RestaurantList },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
+  initialRouteName: 'RestaurantList',
   navigationOptions: {
     headerStyle: styles.header
   }
